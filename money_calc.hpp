9b28adfe762b5bf34e5fe132b4317219be084586
Castedo Ellerman <castedo@castedo.com>
2014-03-12 20:36:55 +0000

    add all the other random comparison combinations

--- money_calc.hpp
+++ money_calc.hpp
@@ -52,16 +52,28 @@ struct money_calc<double>
     return ret;
   }
 
-  bool operator > (money m) const {
-    return minors > m.total_minors() && unit == m.unit();
+  bool operator > (money_calc const& mc) const {
+    if (unit.is_no_currency()) return false;
+    return minors > mc.minors && unit == mc.unit;
   }
 
-  bool operator <= (money m) const {
-    return minors <= m.total_minors() && unit == m.unit();
+  bool operator >= (money_calc const& mc) const {
+    if (unit.is_no_currency()) return false;
+    return minors >= mc.minors && unit == mc.unit;
   }
 
-  //TODO: add more operators
+  bool operator < (money_calc const& mc) const { return mc > *this; }
+  bool operator <= (money_calc const& mc) const { return mc >= *this; }
+  bool operator > (money m) const { return *this > money_calc(m); }
+  bool operator >= (money m) const { return *this >= money_calc(m); }
+  bool operator < (money m) const { return *this < money_calc(m); }
+  bool operator <= (money m) const { return *this <= money_calc(m); }
 };
+
+bool operator > (money m, money_calc<double> const& mc) { return mc < m; }
+bool operator >= (money m, money_calc<double> const& mc) { return mc <= m; }
+bool operator < (money m, money_calc<double> const& mc) { return mc > m; }
+bool operator <= (money m, money_calc<double> const& mc) { return mc >= m; }
 
 inline bool isfinite(money_calc<double> const& mc) { 
   return std::isfinite(mc.minors);
